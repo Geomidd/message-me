6a18715199eeea8a84e5e1f0b841b8e9bb49f7ab
George Middleton <georgemiddleton3@gmail.com>
2022-01-22 18:24:17 -0700

    Add chat scrolling & scroll lock to bottom

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,15 @@ import "@hotwired/turbo-rails";
 import "@doabit/semantic-ui-sass";
 import "./controllers";
 
+window.scroll_bottom = function () {
+  const messages = $("#messages");
+  if (messages.length > 0) {
+    messages.scrollTop(messages[0].scrollHeight);
+  }
+};
+
 $(document).on("turbo:load", function () {
+  scroll_bottom();
   // Setup Semantic UI handlers
   // Message dismissal
   $(".message .close").on("click", function () {
